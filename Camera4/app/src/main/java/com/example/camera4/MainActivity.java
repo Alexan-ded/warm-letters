bef23fde5e5fd2de3fe1219ca5eb6f0da5c94155
Alexanded <alexanderchalkin@yandex.ru>
2023-02-11 14:42:20 +0300

    added http POST request

--- Camera4/app/src/main/java/com/example/camera4/MainActivity.java
+++ Camera4/app/src/main/java/com/example/camera4/MainActivity.java
@@ -30,6 +30,19 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import android.util.Log;
+
+import java.io.ByteArrayOutputStream;
+
+import android.Manifest;
 
 import java.io.File;
 
@@ -42,25 +55,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextureView textureView;
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
-
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-            String msg = "Good good";
-            Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
-        } catch (ActivityNotFoundException e) {
-            // display error state to the user
-        }
-    }
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE) {
-            Bitmap thumbnail = (Bitmap) data.getExtras().get("data"); // this is the picture taken
-
-        }
-    }
+    protected static final int REQUEST_IMAGE_CAPTURE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,16 +66,96 @@ public class MainActivity extends AppCompatActivity {
 
         textureView = (TextureView) findViewById(R.id.view_finder);
 
-        if (allPermissionsGranted()) {
-            findViewById(R.id.imageButton).setOnClickListener(new View.OnClickListener() {// мб тут косяк
-                @Override
-                public void onClick(View view) {
-                    dispatchTakePictureIntent();
-                }
-            });
-        } else {
+        if (!allPermissionsGranted()) {
             ActivityCompat.requestPermissions(this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS);
         }
+        findViewById(R.id.imageButton).setOnClickListener(new View.OnClickListener() {// мб тут косяк
+            @Override
+            public void onClick(View view) {
+                dispatchTakePictureIntent();
+            }
+        });
+    }
+
+//    protected final ActivityResultLauncher<Intent> cameraResultLauncher = registerForActivityResult(
+//            new ActivityResultContracts.TakePicture(),
+//            result -> {
+//                if (result.getResultCode() == RESULT_OK) {
+//                    Bundle extras = result.getData().getExtras();
+//                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+//                    // ...
+//                }
+//            }
+//    );
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            // todo startActivityForResult is not recommended
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+//            cameraResultLauncher.launch(takePictureIntent);
+            String msg = "bebra bebra";
+            Toast.makeText(getBaseContext(), msg, Toast.LENGTH_LONG).show();
+        } catch (ActivityNotFoundException e) {
+            // display error state to the user
+        }
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data"); // this is the picture taken
+
+            // todo here or later image resolution is always 187x250
+
+            // Convert the image to a JPEG format
+            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+            photo.compress(Bitmap.CompressFormat.JPEG, 100, byteArrayOutputStream);
+            byte[] imageBytes = byteArrayOutputStream.toByteArray();
+
+            // Send the image to the server
+            sendImageToServer(imageBytes);
+        }
+    }
+
+    protected void sendImageToServer(final byte[] imageBytes) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    // put your server IP and Port here
+                    URL url = new URL("http://<IP>:<Port>/");
+                    HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("POST");
+                    connection.setDoOutput(true);
+                    connection.setRequestProperty("Content-Type", "image/jpeg");
+                    connection.setRequestProperty("Content-Length", String.valueOf(imageBytes.length));
+
+                    OutputStream outputStream = connection.getOutputStream();
+                    outputStream.write(imageBytes);
+                    outputStream.flush();
+                    outputStream.close();
+
+                    int responseCode = connection.getResponseCode();
+                    if (responseCode == HttpURLConnection.HTTP_OK) {
+                        InputStream inputStream = connection.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                        StringBuilder response = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            response.append(line);
+                        }
+                        reader.close();
+                        inputStream.close();
+                        Log.d("Response", response.toString());
+                    } else {
+                        Log.e("Response Error", "Failed with response code: " + responseCode);
+                    }
+                } catch (IOException e) {
+                    Log.e("Error", e.getMessage(), e);
+                }
+            }
+        }).start();
     }
 
     protected void startCamera() {
