1da10a972aef8dfcc5bef24e27a1363c88a4f5bf
Danila Kulpanovich <kulpich.jun@gmail.com>
2023-05-27 17:11:34 +0300

    animations now operatonal

--- bebr_encoder/main.go
+++ bebr_encoder/main.go
@@ -1,38 +1,68 @@
 package main
 
+import "C"
 import (
 	"image"
 	"image/color"
 	"os"
+	"unsafe"
 )
 
-func main () {
-	// fmt.Println(os.Args[1])
+func minandmax(values []image.Point) (int, int, int, int) {
+	minX := values[0].X
+	maxX := values[0].X 
+	minY := values[0].Y
+	maxY := values[0].Y
+	for _, number := range values {
+		if number.X < minX {
+			minX = number.X
+		}
+		if number.X > maxX {
+			maxX = number.X
+		}
+		if number.Y < minY {
+			minY = number.Y
+		}
+		if number.Y > maxY {
+			maxY = number.Y
+		}
+	}
+	return minX, maxX, minY, maxY
+}
+
+//export file_creator
+func file_creator(numbers *C.int, length C.int, height C.int, width C.int) {
+    go_numbers := (*[1 << 30]C.int)(unsafe.Pointer(numbers))[:length:length]
+	go_length := int(length)
 	pal := color.Palette([]color.Color{
 		color.RGBA{0, 0, 0, 0}, // Transparent
 		color.RGBA{0, 0, 0, 0xff}, // Black
 		color.RGBA{0xff, 0xff, 0xff, 0xff}, // White
 	})
-	// change this into reading bitmap from file
-	var circles [5000]image.Point
+
+	circles := make([]image.Point, go_length / 2)
 	for i := range circles {
-		circles[i].X = i
-		circles[i].Y = i
+		circles[i].X = int(go_numbers[2 * i])
+		circles[i].Y = int(go_numbers[2 * i + 1])
 	}
-	// get from outside
-	w, h := 1000, 1000
+	h, w := int(height), int(width)
 	// change formula
-	pixels_pf := 5 // pixels per frame
+	pixels_pf := 20 // pixels per frame
 	am := BEBR {
-		Frames: make([]Frame, 100),
+		Frames: make([]Frame, go_length / (2 * pixels_pf)),
 		LoopCount: 1,
 	}
-	// all pixels counter, must be less or equal then og pixel counter
-	// all entries of coun, except ones in last im.Set must be removed 
+
 	coun := 0
 	for i := range am.Frames {
-		im := image.NewPaletted(image.Rect(coun, coun, coun + pixels_pf, coun + pixels_pf), pal)
+		minX, maxX, minY, maxY := minandmax(circles[coun:coun+pixels_pf])
+		if (coun > go_length / 2) {
+			break
+		}
+		Xoffset, Yoffset := minX, minY
+		im := image.NewPaletted(image.Rect(0, 0, maxX - minX + 1, maxY - minY + 1), pal)
 		if i == 0 {
+			Xoffset, Yoffset = 0, 0
 			im = image.NewPaletted(image.Rect(0, 0, w, h), pal)
 			for x := 0; x < w; x++ {
 				for y := 0; y < h; y++ {
@@ -44,13 +74,13 @@ func main () {
 		am.Frames[i].Image = im
 		am.Frames[i].DisposeOp = 0
 		am.Frames[i].BlendOp = 1
-		am.Frames[i].XOffset = coun
-		am.Frames[i].YOffset = coun
+		am.Frames[i].XOffset = Xoffset
+		am.Frames[i].YOffset = Yoffset
 		am.Frames[i].DelayNumerator = 1
 		am.Frames[i].DelayDenominator = 240
 
 		for j := 0; j < pixels_pf; j++ {
-			im.Set(circles[coun].X, circles[coun].Y, pal[1])
+			im.Set(circles[coun].X - Xoffset, circles[coun].Y - Yoffset, pal[1])
 			coun++
 		}
 	}
@@ -64,4 +94,65 @@ func main () {
 	if err := Encode(f, am); err != nil {
 		panic(err)
 	}
+}
+
+func main() {
+	// go_length := 100
+
+	// pal := color.Palette([]color.Color{
+	// 	color.RGBA{0, 0, 0, 0}, // Transparent
+	// 	color.RGBA{0, 0, 0, 0xff}, // Black
+	// 	color.RGBA{0xff, 0xff, 0xff, 0xff}, // White
+	// })
+	// circles := make([]image.Point, go_length / 2)
+	// for i := range circles {
+	// 	circles[i].X = i
+	// 	circles[i].Y = i
+	// }
+	// h, w := 1000, 800
+	// // change formula
+	// pixels_pf := 5 // pixels per frame
+	// am := BEBR {
+	// 	Frames: make([]Frame, go_length / (2 * pixels_pf)),
+	// 	LoopCount: 1,
+	// }
+
+	// coun := 0	
+	// for i := range am.Frames {
+	// 	minX, maxX, minY, maxY := minandmax(circles[coun:coun+pixels_pf])
+	// 	if (coun > go_length / 2) {
+	// 		break
+	// 	}
+	// 	Xoffset, Yoffset := minX, minY
+	// 	im := image.NewPaletted(image.Rect(minX, minY, maxX + 1, maxY + 1), pal)
+	// 	if i == 0 {
+	// 		Xoffset, Yoffset = 0, 0
+	// 		im = image.NewPaletted(image.Rect(0, 0, w, h), pal)
+	// 		for x := 0; x < w; x++ {
+	// 			for y := 0; y < h; y++ {
+	// 				im.Set(x, y, pal[2])
+	// 			}
+	// 		}
+	// 	}
+	// 	am.Frames[i].Image = im
+  	// 	am.Frames[i].DisposeOp = 0
+  	// 	am.Frames[i].BlendOp = 1
+  	// 	am.Frames[i].XOffset = Xoffset
+  	// 	am.Frames[i].YOffset = Yoffset
+  	// 	am.Frames[i].DelayNumerator = 1
+  	// 	am.Frames[i].DelayDenominator = 240
+	// 	for j := 0; j < pixels_pf; j++ {
+	// 		im.Set(circles[coun].X, circles[coun].Y, pal[1])
+	// 		coun++
+	// 	}
+	// }
+	// f, err := os.Create("rgb.png")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer f.Close()
+
+	// if err := Encode(f, am); err != nil {
+	// 	panic(err)
+	// }
 }
--- server/server.py
+++ server/server.py
@@ -9,6 +9,7 @@ import cv2 as cv
 import numpy as np
 import yaml
 import subprocess
+import ctypes
 
 
 class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
@@ -29,40 +30,52 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         image_bytes = file_content
 
         if 'multipart/form-data' in content_type:
-            multipart_data = decoder.MultipartDecoder(file_content, content_type).parts
+            multipart_data = decoder.MultipartDecoder(file_content,
+                                                      content_type).parts
             image_bytes = multipart_data[0].content
         elif 'image/jpeg' in content_type:
             image_bytes = file_content
         else:
             print("Error: content_type is not recognized")
             exit()
-        
+
         image_numpy = np.frombuffer(image_bytes, np.int8)
         im = cv.imdecode(image_numpy, cv.IMREAD_UNCHANGED)
         scanner = Scanner()
-        inp = scanner.scan(im) # result from first block
+        inp = scanner.scan(im)  # result from first block
         out = preprocess_img(inp)
-        img_counter = separator(out) # result from second block
-
+        img_counter = separator(out)  # result from second block
+        height, width = out.shape[:2]
         jar_path = './java_exec.jar'
-        process = subprocess.Popen(['java', '-jar', jar_path, str(img_counter)], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        process = subprocess.Popen(['java', '-jar', jar_path, str(img_counter)],
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
         stdout, stderr = process.communicate()
         output = stdout.decode('utf-8')
         number_list = [int(num) for num in output.split()]
-        point_list = [number_list[i:i+2] for i in range(0, len(number_list), 2)]
-        print(point_list[:100])
-        
+        point_list = [number_list[i:i + 2] for i in range(0, len(number_list), 2)]
+
+        lib = ctypes.CDLL('../bebr_encoder/main.so')
+
+        lib.file_creator.argtypes = (ctypes.POINTER(ctypes.c_int),
+                                      ctypes.c_int, ctypes.c_int, ctypes.c_int)
+        c_point_list = (ctypes.c_int * len(number_list))(*number_list)
+        lib.file_creator(c_point_list, len(number_list), height, width)
+
         self.send_response(200)
         self.end_headers()
         print('Success')
 
+
 from os.path import abspath
+
 config_path = '../app/src/main/assets/config/config.yaml'
 try:
     with open(config_path) as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
 except FileNotFoundError:
-    print(f"Error: config file {abspath(config_path)} not found.\nPlease add a config file")
+    print(
+        f"Error: config file {abspath(config_path)} not found.\nPlease add a config file")
     exit()
 
 ip_address = config['ip_address']
